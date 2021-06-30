214d3613d3b763f49dccd976fed1e60035caea4a
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2021-06-30 17:26:34 -0300

    finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { Alert } from 'react-native';
 import { Header } from '../components/Header';
 import { MyTasksList } from '../components/MyTasksList';
 import { TodoInput } from '../components/TodoInput';
@@ -11,18 +11,38 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+
+    if(newTaskTitle !== ''){
+      const data = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      }
+  
+      setTasks(oldState => [...oldState, data]);
+    } else {
+
+      Alert.alert('Você não pode adicionar uma tarefa vazia')
+      return
+    }
+
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const task = tasks.filter((item) => item.id == id)[0];
+    task.done = !task.done;
+
+    const newTasks = [...new Set([task, ...tasks])];
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
